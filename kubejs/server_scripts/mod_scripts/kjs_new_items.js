a307f0d8039c3e9b33b65499073fadba3ebc29f3
Josiah <gtjoe51@gmail.com>
2025-01-07 06:17:36 -0500

    Fixed Blank AI Chips

--- kubejs/server_scripts/mod_scripts/kjs_new_items.js
+++ kubejs/server_scripts/mod_scripts/kjs_new_items.js
@@ -19,7 +19,13 @@ onEvent('recipes', event => {
 		event.shaped('kubejs:energy_crystal_empty', [' A ','BCB',' D '], {A: 'kubejs:large_battery_full',B: 'extendedcrafting:basic_catalyst',C: 'botania:mana_diamond',D: 'immersiveengineering:redstone_acid_bucket'}).id('mbm2:energy_crystal_empty')
 
 		//energy_orb_empty
-		event.shaped('kubejs:energy_orb_empty', ['ABA','BCB','ABA'], {A: 'extendedcrafting:elite_catalyst',B: 'kubejs:energy_crystal_empty',C: 'elementalcraft:purecrystal'}).id('mbm2:energy_orb_empty')
+		event.shaped('kubejs:energy_orb_empty', [
+			'ABA',
+			'BCB',
+			'ABA'
+		], {A: 'extendedcrafting:elite_catalyst',
+			B: 'kubejs:energy_crystal_empty',
+			C: 'elementalcraft:purecrystal'}).id('mbm2:energy_orb_empty')
 
 		//lapatron_crystal_empty
 		event.shaped('kubejs:lapatron_crystal_empty', ['ABA','BCB','ABA'], {A: 'extendedcrafting:ultimate_catalyst',B: 'kubejs:energy_orb_empty',C: 'forbidden_arcanus:dark_nether_star_block'}).id('mbm2:lapatron_crystal_empty')
@@ -28,7 +34,7 @@ onEvent('recipes', event => {
 		event.shaped('kubejs:lapatron_orb_empty', ['ABA','BCB','ABA'], {A: 'extendedcrafting:crystaltine_catalyst',B: 'kubejs:lapatron_crystal_empty',C: 'ae2additions:cell_component_1kkk_1th'}).id('mbm2:lapatron_orb_empty')
 
 		//blank AI chip
-		event.shaped('kubejs:ai_chip', ['ABA','BCB','ABA'], {A: 'kubejs:reinforced_hdpe_sheet',B: 'kubejs:tier_3_electrical_alloy_wire',C: 'pneumaticcraft:unassembled_pcb'}).id('mbm2:blank_ai_chip')
+		event.shaped('kubejs:blank_ai_chip', ['ABA','BCB','ABA'], {A: 'kubejs:reinforced_hdpe_sheet',B: 'kubejs:tier_3_electrical_alloy_wire',C: 'pneumaticcraft:unassembled_pcb'}).id('mbm2:blank_ai_chip')
 		
 		//ai_chip
 		event.shaped('kubejs:ai_chip', ['ABA','BCB','ABA'], {A: 'hostilenetworks:empty_prediction',B: 'laserio:logic_chip',C: 'kubejs:blank_ai_chip'}).id('mbm2:ai_chip')

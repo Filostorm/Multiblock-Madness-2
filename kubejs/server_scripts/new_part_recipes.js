ca942d6e68555dd4279072a115f30e9b9c2ed215
TheSmartNoob880 <109487391+TheSmartNoob880@users.noreply.github.com>
2025-01-17 22:48:26 -0500

    Update new_part_recipes.js

--- kubejs/server_scripts/new_part_recipes.js
+++ kubejs/server_scripts/new_part_recipes.js
@@ -443,6 +443,13 @@ onEvent('recipes', event => {
 			  ], {
 				R: `#forge:rings/${item.material}`,
 			  }).id(`mbm2:crafting/${item.material}_sprockets`)
+			//Cheaper recipe
+			event.recipes.multiblocked.multiblock("mechanical_crafting")
+			.inputItem(`3x #forge:rings/${item.material}`)
+			.outputItem(Item.of(`#forge:sprockets/${item.material}`))
+			.setPerTick(true)
+			.inputFE(2000)
+			.duration(100)
 		}
 	}
 
